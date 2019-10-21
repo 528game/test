878089790400d69f999c888ac32a359c0e40674f
528game <56824153+528game@users.noreply.github.com>
2019-10-21 23:01:36 +0800

    Create jsGetCode.js

---
+++ jsGetCode.js
@@ -0,0 +1,15 @@
+OnGetCode = function ( nType )
+{
+    let strCode = "";
+    //public
+    if( nType == 0 )
+    {
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUxC91Q7bmKIfhodm61VMcduPMgelizWyWnt43Hdz4togpKxk2EqCnC9Sdm6wSwrPPmrl69Q+eDgud5GNGH9jXWO1lrzMhbMYxwLuuG0+6BOuw==";
+    }
+    //Dev
+    else
+    {
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUxC91Q7bmKIfhodm61VMcduPMgelizWyWnt43Hdz4togpKxk2EqCnC9Sdm6wSwrPPmrl69Q+eDgud5GNGH9jXWO1lrzMhbMYxwLuuG0+6BOuw==";
+    }
+    return  strCode;
+}
